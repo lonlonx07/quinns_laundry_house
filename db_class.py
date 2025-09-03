f99ffc4c538311cdcf8131465d06032f73ef4ae4
Marlon Medina <lonlonx07@gmail.com>
2025-09-03 17:09:17 +0800

    update db_class.py

--- db_class.py
+++ db_class.py
@@ -7,16 +7,16 @@ import json
 import base64
 import firebase_admin
 from firebase_admin import credentials, messaging
-#cred = credentials.Certificate("serviceAccountKey.json")
-#firebase_admin.initialize_app(cred)
-
-encoded_key = os.getenv("SERVICE_ACCOUNT_JSON")
-if not encoded_key:
-    raise ValueError("The SERVICE_ACCOUNT_JSON environment variable is not set.")
-
-service_account_info = json.loads(base64.b64decode(encoded_key).decode('utf-8'))
-cred = credentials.Certificate(service_account_info)
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+# encoded_key = os.getenv("SERVICE_ACCOUNT_JSON")
+# if not encoded_key:
+#     raise ValueError("The SERVICE_ACCOUNT_JSON environment variable is not set.")
+
+# service_account_info = json.loads(base64.b64decode(encoded_key).decode('utf-8'))
+# cred = credentials.Certificate(service_account_info)
+# firebase_admin.initialize_app(cred)
 
 import pytz
 UTC = pytz.utc
